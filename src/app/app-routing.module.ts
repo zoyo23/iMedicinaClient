c3cc732c81a9b700221777f233325b87da515a56
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-03-22 23:21:38 -0300

    definição de novas rotas para criação e listagem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CriarClienteComponent } from './views/Clientes/criar-cliente/criar-cliente.component';
+import { ListarClientesComponent } from './views/Clientes/listar-clientes/listar-clientes.component';
+import { CriarSalaComponent } from './views/Salas/criar-sala/criar-sala.component';
+import { ListarSalasComponent } from './views/Salas/listar-salas/listar-salas.component';
 
 
 const routes: Routes = [
   { path: 'novoCliente', component: CriarClienteComponent },
+  { path: 'listaClientes', component: ListarClientesComponent },
+  { path: 'novaSala', component: CriarSalaComponent },
+  { path: 'listaSalas', component: ListarSalasComponent },
 ];
 
 @NgModule({
