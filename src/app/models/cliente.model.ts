bb4262f4e0ff81409b1a7de40291c4529ed20c77
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-03-22 23:47:07 -0300

    listagem de clientes cadastrados

--- src/app/models/cliente.model.ts
+++ src/app/models/cliente.model.ts
@@ -1,6 +1,7 @@
 export class Cliente {
 
     public constructor(
+        public Id: string,
         public Cpf: string,
         public Nome: string
     ) { }
--- src/app/views/Clientes/listar-clientes/listar-clientes.component.ts
+++ src/app/views/Clientes/listar-clientes/listar-clientes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Cliente } from 'src/app/models/cliente.model';
 
 @Component({
   selector: 'app-listar-clientes',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarClientesComponent implements OnInit {
 
-  constructor() { }
+  public listaClientes: Cliente[];
+
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+
+    this.httpClient.get(`${environment.url_base_api}/api/Clientes`)
+      .subscribe(t => {
+        this.listaClientes = t;
+
+        console.log(this.listaClientes);
+      });
+
   }
 
 }
